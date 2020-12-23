161ab76ea568379502f8d392c8b4fe9f6a1ceba8
wjdgurrj <wjdgurrj25@gmail.com>
2020-12-24 04:11:06 +0900

    learned at 12/24

---
+++ console.js
@@ -0,0 +1,9 @@
+﻿console.time('Timer');
+
+var sum = 0;
+for(var i = 1; i<10000;i++){
+    sum += 1;
+}
+
+console.log("sum :", sum);
+console.timeEnd("Timer");
--- customConsole.js
+++ customConsole.js
@@ -1,2 +1,14 @@
-﻿var Console = require('console').Console;
+﻿var fs = require('fs');
+var output = fs.createWriteStream("stdout.log");
+var errorOutput = fs.createWriteStream('error.log');
+
+var Console = require('console').Console;
+var logger = new Console(output, errorOutput);
+
+logger.info("info message");
+logger.log("log message");
+
+logger.warn('warning');
+logger.error('error message');
+
 
