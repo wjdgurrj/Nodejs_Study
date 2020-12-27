344982f9d51e740cd14d11927f967e77f6005bfe
wjdgurrj <wjdgurrj25@gmail.com>
2020-12-27 17:23:42 +0900

    체인방식의 promise로 비동기 동작 제어

---
+++ ch06/promise.js
@@ -0,0 +1,25 @@
+﻿function task1 (fulfill, reject){
+    console.log("Task1 시작");
+    setTimeout(function(){
+        console.log('Task1 끝');
+        fullfilled("Task1 결과")
+        reject("error msg");
+    },300);
+}
+
+function Task2 (fulfill, reject){
+    console.log("Task2 시작");
+    setTimeout(function(){
+        console.log('Task2 끝');
+        reject("Task2 결과");
+    },300);
+}
+
+function fullfilled(result){
+    console.log("fullfilled : ", result);
+}
+function rejected(err){
+    console.log("rejected : ", err);
+}
+
+new Promise(task1).then(fullfilled, rejected);
