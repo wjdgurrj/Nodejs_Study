afd4e75e9f401727f3eaf40d64c4a4dca86c1137
wjdgurrj <wjdgurrj25@gmail.com>
2021-01-15 18:17:34 +0900

    router를 사용한 error 처리 실습

---
+++ http_practice/err.js
@@ -0,0 +1,21 @@
+﻿var express = require("express");
+var app = express();
+
+app.get('/:value', work);
+app.use(errorHandler);
+app.listen(3000);
+
+function work(req,res,next){
+    var val = parseInt(req.params.value);
+
+    if(!val){
+        res.status(400).send("입력값이 숫자가 아닙니다.")
+        next(error);
+        return ;
+    }
+    res.send('Result : ' + val);
+}
+
+function errorHandler(err,req,ers,next){
+    res.send("Error 발생");
+}
