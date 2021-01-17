e5cf405f3d0bdf9ecfce1654bceada6b5d814f69
wjdgurrj <wjdgurrj25@gmail.com>
2021-01-17 20:33:35 +0900

    ejsServer 동작 및 엔진활용 학습

---
+++ http_practice/ejsServer.js
@@ -0,0 +1,17 @@
+﻿var express = require('express');
+var app = express();
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+var data = [
+    {title : 'cat1', image: 'cat1.png'},
+    {title : 'cat2', image: 'cat2.png'},
+    {title : 'cat3', image: 'cat3.png'}
+];
+app.use(express.static('./'));
+app.get('/', function(req,res){
+    res.render('cat', {title:'Cats', cats:data})
+});
+
+app.listen(3000);
