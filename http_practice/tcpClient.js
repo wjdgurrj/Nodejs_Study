0868713949542d31f3e2a40872df84105f59d22e
wjdgurrj <wjdgurrj25@gmail.com>
2021-02-03 15:28:11 +0900

    tcp 실시간 통신 실습

---
+++ http_practice/tcpClient.js
@@ -0,0 +1,20 @@
+﻿var net = require('net');
+
+var ip = '127.0.0.1';
+var port = 3000;
+
+var socket = new net.Socket();
+socket.connect({host:ip, port:port}, function(){
+    console.log('서버와 연결 성공');
+
+    socket.write("hello socket server\n");
+    socket.end();
+
+    socket.on('data', function(chunk){
+        console.log("서버가 보냄 : ", chunk.toString());
+    });
+
+    socket.on('end', function(){
+        console.log('서버 연결 종료');
+    });
+});
---
+++ http_practice/tcpServer.js
@@ -0,0 +1,22 @@
+﻿var net = require('net');
+var server = net.createServer(function(socket){
+    console.log("클라이언트 접속");
+    socket.write("welcome to Socket Server");
+
+    socket.on('data', function(chunk){
+        console("클라이언트가 보냄 : ", chunk.toString());
+    });
+
+    socket('end', function(){
+        console.log("클라이언트 접속 종료");
+    });
+});
+
+server.on('listening', function(){
+    console.log("server is listening");
+});
+server.on('close', function(){
+    console.log('server closed');
+});
+
+server.listen(3000);
