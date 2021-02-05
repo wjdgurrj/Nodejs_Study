ce50f7edf06944d71e8631a75b9ff4b5690d7fea
wjdgurrj <wjdgurrj25@gmail.com>
2021-02-05 19:47:33 +0900

    udpSender 학습

---
+++ http_practice/udpSender.js
@@ -0,0 +1,14 @@
+﻿const { Buffer } = require('buffer');
+var dgram = require('dgram');
+var socket = dgram.createSocket('udp4');
+
+var msg = new Buffer('Hello UDP Receiver');
+socket.send(msg, 0, msg.length, 3000, '127.0.0.1', function(err){
+    console.log(err);
+    if(err){
+        console.log("UDP message send error");
+        return;
+    }
+    console.log("메세지 전송 성공");
+    socket.close();
+});
